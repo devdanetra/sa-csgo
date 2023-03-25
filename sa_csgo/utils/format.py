9cd08f1e069871bd38591d9c0c0b131741a6ded2
devdanetra <devdanetra@outlook.com>
2023-03-25 11:39:54 +0100

    corrected readme

--- sa_csgo/utils/format.py
+++ sa_csgo/utils/format.py
@@ -1,7 +1,11 @@
+import importlib
 from rich import print
 from rich.panel import Panel
 from rich.align import Align
 from rich import box
+
+def version():
+    return importlib.metadata.version('sa-csgo')
 
 def printLogo(): 
     title = ("[bold blue]\n" +
@@ -10,5 +14,5 @@ def printLogo():
     "███████╗•███████║█████╗██║     ███████╗╚═╝██║  ███╗██║   ██║\n" +
     "╚════██║ ██╔══██║╚════╝██║     ╚════██║██╗██║   ██║██║   ██║\n" +
     "███████║ ██║  ██║      ╚██████╗███████║╚═╝╚██████╔╝╚██████╔╝\n" +
-    "╚══════╝ ╚═╝  ╚═╝ [bold white]v1.00 [/bold white]╚═════╝╚══════╝    ╚═════╝  ╚═════╝ \n")
+    f"╚══════╝ ╚═╝  ╚═╝ [bold white] <{version()}> [/bold white]╚═════╝╚══════╝    ╚═════╝  ╚═════╝ \n")
     print(Panel(Align(title,align='center'), title="[bold white][MIT Licensed] by STORM Systems    ||   <info@stormsys.io>", title_align='center', box=box.SIMPLE_HEAVY))
